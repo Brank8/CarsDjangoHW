5015e21479c7461301b68a467cc68ec2b89e0dd9
Branko <branko.stanisic02@gmail.com>
2024-01-11 14:50:35 -0500

    Complete Part3 HW

--- carscollector/main_app/models.py
+++ carscollector/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'car_id': self.id})
